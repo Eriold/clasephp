07b323dcb866b4e1ab96364351efb66a900a9ad6
Daniel <dacamoty@gmail.com>
2022-09-02 17:38:12 -0500

    error added

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="mt-5 mb-3 clearfix">
-                        <h2 class="pull-left">Employees Details</h2>
-                        <a href="./view/create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Add New Employee</a>
+                        <h2 class="pull-left">Detalles de personas</h2>
+                        <a href="./view/create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i>Agregar una persona</a>
                     </div>
                     <?php
                     // Include config file
