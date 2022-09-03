b113ae955719dba4ea6599b299091dc351f510fb
Daniel <dacamoty@gmail.com>
2022-09-03 11:26:12 -0500

    Space in add people in index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
                 <div class="col-md-12">
                     <div class="mt-5 mb-3 clearfix">
                         <h2 class="pull-left">Detalles de personas</h2>
-                        <a href="./view/create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i>Agregar una persona</a>
+                        <a href="./view/create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> Agregar una persona</a>
                     </div>
                     <?php
                     // Include config file
